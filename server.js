6d8faa41503060f9bfd788b574c99a7a172bba99
Surya <suryab.2392000@gmail.com>
2022-10-23 20:34:57 +0530

    c

--- server.js
+++ server.js
@@ -5,20 +5,10 @@ const express = require("express");
 const app = express();
 const jwt = require('jsonwebtoken')
 const cors = require('cors');
+import routes from "./ServerRoute/index.js";
+
 import pool  from './db.js';
-import axios from 'axios';
-import bcrypt from 'bcrypt';
-import {google} from 'googleapis'
-import { query } from "express";
 app.use(cors());
-let L = ['100-0', '80-20', '70-30', '50-50', '30-70', '20-80'];
-let x = [1800, 1900, 2000]
-let y = [1200, 1300, 1400]
-const teamRatio = ['1-5', '1-7', '1-10', '1-12']
-const teamLeadSalary = [2500, 2600, 2700]
-const projectDuration = 5
-const teamLeadRatio = ['1-5', '1-7', '1-10', '1-12']
-const pmSalary = [3500, 3600, 3700]
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname,"client/build")))
 
@@ -27,6 +17,9 @@ app.use(express.static(path.join(__dirname,"client/build")))
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000;
+routes.forEach(route => {
+    app[route.method](route.path, route.handler);
+});
 
 
 
