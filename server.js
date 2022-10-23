14e366f6377f53a8b7390b975545211dd66c5035
Surya <suryab.2392000@gmail.com>
2022-10-23 20:50:39 +0530

    c

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const jwt = require('jsonwebtoken')
 const cors = require('cors');
-import routes from "./ServerRoute/index.js";
+// import routes from "./ServerRoute/index.js";
 
 import pool  from './db.js';
 app.use(cors());
