43eb48776de889a847afdbf688f819519be4b12d
Surya <suryab.2392000@gmail.com>
2022-10-24 08:39:59 +0530

    c

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import signup from "./ServerRoute/Signup.js";
+// import signup from "./ServerRoute/Signup.js";
 // import login from "./ServerRoute/login.js";
-
+import sig from "./ServerRoute/Signup.js";
 const routes = 'hi'
 
 export default routes;
